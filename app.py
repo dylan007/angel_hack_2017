5d22ad93196d5eac70ebc326ad298b0228b16275
pranavtharoor <pranav.tharoor@gmail.com>
2017-04-08 16:10:09 +0530

    added hackerrank

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from bs4 import BeautifulSoup
 import urllib2
 import re
 import json
-
-
-
+import subprocess
 
 app = Flask(__name__)
 
@@ -13,9 +11,8 @@ app = Flask(__name__)
 def index():
 	return "Hello World"
 
-
-@app.route('/user/<ch>/<cf>')
-def get_data(cf,ch):
+@app.route('/user/<ch>/<cf>/<hr>')
+def get_data(cf,ch, hr):
 	url = 'https://www.codechef.com/users/' + ch
 	response = urllib2.urlopen(url).read()
 	p_res = BeautifulSoup(response,'html.parser')
@@ -31,9 +28,25 @@ def get_data(cf,ch):
 		codeforces = int(p_res["result"][0]["rating"])
 	else:
 		codeforces=0
+
+	proc = subprocess.Popen(["curl https://www.hackerrank.com/" + hr + "?hr_r=1", "~"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	arr = out.split('%22')
+	arr_i = 0
+	count = 0
+	for ele in arr:
+		count += 1
+		if "score" in ele:
+			arr_i += 1
+		if arr_i == 16:
+			hackerrank = arr[count]
+			break
+	hackerrank = hackerrank[3:-3]
+
 	out = {}
 	out["codechef"] = codechef
 	out["codeforces"] = codeforces
+	out["hackerrank"] = hackerrank
 	return json.dumps(out)
 
 if __name__=='__main__':
